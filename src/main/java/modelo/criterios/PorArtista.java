07038c9cf9775f4d92435f30b9945e2c4f65fad5
rchgst <quispericardo682@gmail.com>
2026-05-23 17:29:26 -0300

    add order criterio structure

--- src/main/java/modelo/criterios/PorArtista.java
+++ src/main/java/modelo/criterios/PorArtista.java
@@ -6,7 +6,6 @@ import modelo.datos.Cancion;
 public class PorArtista implements CriterioOrdenacion{
 
     public int comparar(Cancion c1, Cancion c2) {
-        //if (c1.getArtista.compareTo())//completar jsdjsj
-        return 0;
+        return c1.getArtista().compareToIgnoreCase(c2.getArtista());
     }
 }
