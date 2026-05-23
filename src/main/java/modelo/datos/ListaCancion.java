c7746681f6c8746b82d1d72938b817e0e5b145a2
rchgst <quispericardo682@gmail.com>
2026-05-23 19:45:04 -0300

    add list class

---
+++ src/main/java/modelo/datos/ListaCancion.java
@@ -0,0 +1,16 @@
+package modelo.datos;
+
+import modelo.estructuras.Lista1DLinkedL;
+
+public class ListaCancion extends Lista1DLinkedL {
+
+    @Override
+    public boolean esIgual(Object a, Object b) {
+        a = (Cancion)a;
+        b = (Cancion)b;
+        boolean c1 = ((Cancion) a).getAnio() == ((Cancion) b).getAnio();
+        boolean c2 = ((Cancion) a).getArtista().equals(((Cancion) b).getArtista());
+        boolean c3 = ((Cancion) a).getTitulo().equals(((Cancion) b).getTitulo());
+        return c1 && c2 && c3;
+    }
+}
