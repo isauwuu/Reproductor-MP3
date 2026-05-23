447fc398e0333c0dd64755f1c57994bdaac3cec0
isa uwu <isacacharani@gmail.com>
2026-05-23 16:40:49 -0300

    Se agregó el atributo de la duracion de la cancion en segundos, su getter y un getter especial q devuelve la duracion en formato "xx:xx"
    # Conflicts:
    #	src/main/java/modelo/datos/Cancion.java

--- src/main/java/modelo/datos/Cancion.java
+++ src/main/java/modelo/datos/Cancion.java
@@ -9,22 +9,24 @@ import java.io.File;
 public class Cancion {
     private String titulo;
     private String artista;
-    private String anio;
+    private int anio;
     private String rutaArchivo;
     private Image portada;
     private boolean valida;
+    private long duracionSegundos;
 
     public Cancion(String rutaArchivo) {
         this.rutaArchivo = rutaArchivo;
         this.titulo = "Desconocido";
         this.artista = "Desconocido";
-        this.anio = "Desconocido";
+        this.anio = -1;
+        this.duracionSegundos=0;
         this.portada = null;
         this.valida = false;
         cargaMetadatos();
     }
 
-    private void asignarMetadatosBasicos(String titulo, String artista, String anio) {
+    private void asignarMetadatosBasicos(String titulo, String artista, int anio) {
 
         //.trim saca los espacios de los costados, sirve para asegurarse q el titulo no este vacio nomas "   " -> ""
         //es mas q nada porq muchos mp3 tienen los campos de los metadatos grabados pero con espacios vacios nomas
@@ -35,7 +37,7 @@ public class Cancion {
         if ((artista != null) && (!artista.trim().isEmpty()))
             this.artista = artista;
 
-        if ((anio != null) && (!anio.trim().isEmpty()))
+        if (anio != -1)
             this.anio = anio;
     }
 
@@ -44,22 +46,26 @@ public class Cancion {
             File archivo = new File(this.rutaArchivo);
             if (!archivo.exists())
                 return;
+
             //si el archivo existe pero no tiene metadatos se usa el nombre del archivo como título en vez de no mostrar nada o mostrar "artista desconocido
             //es simplemente para q el usuario tenga mas contexto(?
+
             this.titulo = archivo.getName().replaceAll("(?i)\\.mp3$", "");
             Mp3File mp3 = new Mp3File(this.rutaArchivo);
+
+            this.duracionSegundos = mp3.getLengthInSeconds();
+
             if (mp3.hasId3v2Tag()) {
                 ID3v2 tag = mp3.getId3v2Tag();
-                asignarMetadatosBasicos(tag.getTitle(), tag.getArtist(), tag.getYear());
+                asignarMetadatosBasicos(tag.getTitle(), tag.getArtist(), Integer.parseInt(tag.getYear()));
 
                 byte[] portadaBytes = tag.getAlbumImage();
                 if (portadaBytes != null)
                     this.portada = new Image(new ByteArrayInputStream(portadaBytes));
 
-
             } else if (mp3.hasId3v1Tag()) {
                 ID3v1 tag = mp3.getId3v1Tag();
-                asignarMetadatosBasicos(tag.getTitle(), tag.getArtist(), tag.getYear());
+                asignarMetadatosBasicos(tag.getTitle(), tag.getArtist(), Integer.parseInt(tag.getYear()));
                 // ID3v1 no tiene portada
             }
             this.valida = true;
@@ -78,7 +84,7 @@ public class Cancion {
         return rutaArchivo;
     }
 
-    public String getAnio() {
+    public int getAnio() {
         return anio;
     }
 
@@ -98,14 +104,21 @@ public class Cancion {
         this.artista = artista;
     }
 
-    public void setAnio(String anio) {
+    public void setAnio(int anio) {
         this.anio = anio;
     }
 
     public void setPortada(Image portada) {
         this.portada = portada;
     }
-
+    public String getDuracionFormateada(){
+        long minutos = this.duracionSegundos/60;
+        long segundos = this.duracionSegundos % 60;
+        return String.format("%02d:%02d", minutos, segundos);
+    }
+    public long getDuracionSegundos() {
+        return duracionSegundos;
+    }
     @Override
     public String toString() {
         return "Cancion{" +
