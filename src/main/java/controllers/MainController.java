936bcac60be71e7506244b0d134891b4d9dd6599
rchgst <quispericardo682@gmail.com>
2026-05-28 12:22:33 -0300

    create structure for events manager

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,7 +1,44 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class MainController {
 
+    @FXML
+    void loopEventButon(ActionEvent event) {
 
+    }
+
+    @FXML
+    void pausedEventButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playEventButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousSongEventButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void shuffleEventButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void skipSongEventButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volumenEventSlideBar(MouseEvent event) {
+
+    }
 
 }
